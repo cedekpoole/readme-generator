29347c73c7bbd67e165d1db6f2e7db63ebf159a8
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-17 23:45:26 +0000

    create template literal for 'generateMarkdown' function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,48 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${data.badgeLicense}
+
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing-here-is-a-header)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+    
+## Contributing
+
+${data.contribute}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. 
+You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+Feel free to also add me on LinkedIn: ${data.linkedIn} :)
+
+
 `;
 }
 
