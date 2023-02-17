710d6eb0d991bd0dcebc496f1c54350b4e368461
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-17 20:12:47 +0000

    add additional packages to package.json file

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
+const colours = require("colors");
+const figlet = require("figlet");
+
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?'.green,
+        name: 'title'
+    }
 
 ];
+
+inquirer.prompt(questions)
 
 // function to write README file
 function writeToFile(fileName, data) {
