2cfda06ec3c54d43e6f85b8454437ec9005b5b90
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-18 00:52:55 +0000

    add additional questions for inquirer prompts

--- utils/questions.js
+++ utils/questions.js
@@ -1,30 +1,41 @@
 // array of questions for user
 const questions = [
-    {
-      type: "input",
-      message: "What is your project title?".green,
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Enter a project description:".green,
-      name: "description",
-    },
-    {
-      type: "list",
-      message: "Please select the license:".green,
-      name: "license",
-      choices: [
-        "MIT",
-        "ISC",
-        "Mozilla",
-        "Apache 2.0",
-        "Eclipse Public License",
-        "None of the above",
-      ],
-    },
-  ];
+  {
+    type: "input",
+    message: "What is your project title?".green,
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Enter a project description:".green,
+    name: "description",
+  },
+  {
+    type: "list",
+    message: "Please select the license:".green,
+    name: "license",
+    choices: [
+      "MIT",
+      "ISC",
+      "Mozilla",
+      "Apache 2.0",
+      "Eclipse Public License",
+      "None of the above",
+    ],
+  },
+  {
+    type: "input",
+    message: "Enter installation information:".green,
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Enter usage information:".green,
+    name: "usage",
+  },
+  
+];
 
-  module.exports = {
-    questions
-  }
+module.exports = {
+  questions,
+};
