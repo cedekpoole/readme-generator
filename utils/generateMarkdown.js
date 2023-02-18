e0ed4d827efbe53ded290384d995856a5ee3bbc6
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-02-18 14:30:08 +0000

    edit 'questions' section of generated readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -40,7 +40,7 @@ ${data.test}
 ## Questions
 
 If you have any questions about the repo, open an issue or contact me directly at ${data.email}. 
-You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+You can find more of my work by clicking on my GitHub username: [${data.github}](https://github.com/${data.github}/).
 Feel free to also add me on [LinkedIn](${data.linkedIn}). :)
 
 
